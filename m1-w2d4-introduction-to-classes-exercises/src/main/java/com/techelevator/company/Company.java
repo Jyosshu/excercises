e5ef4b1117575e291a6f742a96af312a8ec5edf5
Josh Wygle <jwygle@gmail.com>
2017-09-08 14:13:41 -0400

    homework upload

--- m1-w2d4-introduction-to-classes-exercises/src/main/java/com/techelevator/company/Company.java
+++ m1-w2d4-introduction-to-classes-exercises/src/main/java/com/techelevator/company/Company.java
@@ -2,8 +2,58 @@ package com.techelevator.company;
 
 public class Company {
 
-	//
-	// Write code here
-	//
+private String name;
+private int numberOfEmployees = 0;
+private double revenue = 0.00;
+private double expenses = 0.00;
+
+public String getCompanySize() {
+	if (numberOfEmployees > 250) {
+		return "large";
+	}
+	else if (numberOfEmployees > 50 && numberOfEmployees <= 250 ) {
+		return "medium";
+	}
+	else {
+		return "small";
+	}
+}
+
+public double getProfit() {
+	return revenue - expenses;
+}
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+public int getNumberOfEmployees() {
+	return numberOfEmployees;
+}
+
+public void setNumberOfEmployees(int numberOfEmployees) {
+	this.numberOfEmployees = numberOfEmployees;
+}
+
+public double getRevenue() {
+	return revenue;
+}
+
+public void setRevenue(double revenue) {
+	this.revenue = revenue;
+}
+
+public double getExpenses() {
+	return expenses;
+}
+
+public void setExpenses(double expenses) {
+	this.expenses = expenses;
+}
 	
 }
+
--- m1-w2d4-introduction-to-classes-exercises/src/main/java/com/techelevator/dog/Dog.java
+++ m1-w2d4-introduction-to-classes-exercises/src/main/java/com/techelevator/dog/Dog.java
@@ -2,8 +2,31 @@ package com.techelevator.dog;
 
 public class Dog {
 
-	//
-	// Write code here
-	//
+	private boolean sleeping = false;
 	
+	public String makeSound() {
+		if (sleeping == true) {
+			return "Zzzzz...";
+		}
+		else {
+			return "Woof!";
+		}
+	}
+	
+	public boolean isSleeping() {
+		if (sleeping == true) {
+		return sleeping = true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public void sleep() {
+		sleeping = true;
+	}
+	
+	public void wakeUp() {
+		sleeping = false;
+	}
 }
