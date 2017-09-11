14215209c315af2d5ab3b550a8264e080516789f
Josh Wygle <jwygle@gmail.com>
2017-09-11 15:38:47 -0400

    commiting exercises

--- m1-w3d1-oop-with-encapsulation-exercises/src/main/java/com/techelevator/FruitTree.java
+++ m1-w3d1-oop-with-encapsulation-exercises/src/main/java/com/techelevator/FruitTree.java
@@ -1,5 +1,31 @@
 package com.techelevator;
 
 public class FruitTree {
+	private String typeOfFruit;
+	private int piecesOfFruitLeft;
+	
+	public boolean pickFruit(int numberOfPiecesToRemove) {
+		if (piecesOfFruitLeft >= numberOfPiecesToRemove) {
+			this.piecesOfFruitLeft -= numberOfPiecesToRemove;
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public FruitTree(String typeOfFruit, int startingPiecesOfFruit) {
+		 this.typeOfFruit = typeOfFruit;
+		 this.piecesOfFruitLeft = startingPiecesOfFruit;
+	}
+	
+	public String getTypeOfFruit() {
+		return typeOfFruit;
+	}
+	public int getPiecesOfFruitLeft() {
+		return piecesOfFruitLeft;
+	}
+	
+	
 
 }
--- m1-w3d1-oop-with-encapsulation-exercises/src/main/java/com/techelevator/HomeworkAssignment.java
+++ m1-w3d1-oop-with-encapsulation-exercises/src/main/java/com/techelevator/HomeworkAssignment.java
@@ -1,5 +1,49 @@
 package com.techelevator;
 
 public class HomeworkAssignment {
+	
+	private int totalMarks;
+	private int possibleMarks;
+	private String submitterName;
+	
+	public HomeworkAssignment(int possibleMarks) {
+		this.possibleMarks = possibleMarks;
+	}
+	
+	public String getLetterGrade() {
+		if (((totalMarks * 100) / possibleMarks) >= 90) {
+			return "A";
+		}
+		else if (((totalMarks * 100) / possibleMarks) >= 80 && ((totalMarks * 100) / possibleMarks) < 90) {
+			return "B";
+		}
+		else if (((totalMarks * 100) / possibleMarks) >= 70 && ((totalMarks * 100) / possibleMarks) < 80) {
+			return "C";
+		}
+		else if (((totalMarks * 100) / possibleMarks) >= 60 && ((totalMarks * 100) / possibleMarks) < 70) {
+			return "D";
+		}
+		else {
+			return "F";
+		}
+	}
+	
+	public int getTotalMarks() {
+		return totalMarks;
+	}
+	public void setTotalMarks(int totalMarks) {
+		this.totalMarks = totalMarks;
+	}
+	public String getSubmitterName() {
+		return submitterName;
+	}
+	public void setSubmitterName(String submitterName) {
+		this.submitterName = submitterName;
+	}
+	public int getPossibleMarks() {
+		return possibleMarks;
+	}
+	
+	
 
 }
