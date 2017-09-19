0b3e72664e1755645da621674b95bb73bcb55fe7
Josh Wygle <jwygle@gmail.com>
2017-09-19 18:02:34 -0400

    w4d1 Fizzbuzz excercise.

--- m1-w4d1-tdd-exercises/src/main/java/com/techelevator/KataFizzBuzz.java
+++ m1-w4d1-tdd-exercises/src/main/java/com/techelevator/KataFizzBuzz.java
@@ -1,5 +1,55 @@
 package com.techelevator;
 
 public class KataFizzBuzz {
+	private String result = "";
+	// private int number = 0;
+	
+	public String fizzBuzz(int number) {
+		
+		if (number >= 1 && number <= 100) {
+			if (number % 3 == 0 || number % 5 == 0 || Integer.toString(number).contains("3") || Integer.toString(number).contains("5")) {
+				if (number % 3 == 0 || Integer.toString(number).contains("3")) {
+					if (number % 5 == 0) {
+						result = "FizzBuzz";
+						return result;
+					}
+					else{
+					result = "Fizz";
+					return result;
+					}
+				}
+				else {
+					if (number % 3 == 0) {
+						result = "FizzBuzz";
+						return result;
+					}
+					else {
+						if (Integer.toString(number).contains("5")) {
+							result = "Buzz";
+							return result;
+						}
+						else {
+					result = "Buzz";
+					return result;
+						}
+					}
+				}
+			}
+			else {
+				result = Integer.toString(number);
+				// System.out.println(result);
+				return  result;
+			}	
+		}
+		else {
+			return "Not a valid entry.";
+		}
+	}
+	
+	public void fizzBuzzOutput(int number) {
+		for (int i = 0; i <= number; i++) {
+			System.out.println(fizzBuzz(i));
+		}
+	}
 
 }
--- m1-w4d1-tdd-exercises/src/test/java/com/techelevator/KataFizzBuzzTest.java
+++ m1-w4d1-tdd-exercises/src/test/java/com/techelevator/KataFizzBuzzTest.java
@@ -4,5 +4,40 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class KataFizzBuzzTest {
+	
+	KataFizzBuzz sut;
+	
+	@Before
+	public void setup() {
+		sut = new KataFizzBuzz();
+	}
 
+	@Test
+	public void testGreaterThanZero() {
+		String result = sut.fizzBuzz(0);
+		assertEquals("Not a valid entry.", result);
+	}
+	
+	@Test
+	public void testOne() {
+		String result = sut.fizzBuzz(1);
+		assertEquals("1", result);
+	}
+	
+	@Test
+	public void testUpperLimit() {
+		String result = sut.fizzBuzz(100);
+		assertEquals("Buzz", result);
+	}
+	
+	@Test
+	public void testPastUpperLimit() {
+		String result = sut.fizzBuzz(101);
+		assertEquals("Not a valid entry.", result);
+	}
+	
+	@Test
+	public void testFizzBuzzOutput() {
+		sut.fizzBuzzOutput(53);
+	}
 }
