e39aa9cdcbaefd99c01f2b18ce8f0cfbb1d3d977
Josh Wygle <jwygle@gmail.com>
2017-09-20 16:26:49 -0400

    commit for FizzWriter

--- m1-w4d3-file-io-part2-exercises/src/main/java/com/techelevator/FizzWriter.java
+++ m1-w4d3-file-io-part2-exercises/src/main/java/com/techelevator/FizzWriter.java
@@ -1,10 +1,73 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 public class FizzWriter {
+	
+	private static String result = "";
+	// private int number = 0;
 
 	public static void main(String[] args) {
 		
-
+		fizzBuzzOutput(300);
+		
 	}
 
+	public static String fizzBuzz(int number) {
+		
+		if (number >= 1 && number <= 300) {
+			if (number % 3 == 0 || number % 5 == 0 || Integer.toString(number).contains("3") || Integer.toString(number).contains("5")) {
+				if (number % 3 == 0 || Integer.toString(number).contains("3")) {
+					if (number % 5 == 0) {
+						result = "FizzBuzz";
+						return result;
+					}
+					else{
+					result = "Fizz";
+					return result;
+					}
+				}
+				else {
+					if (number % 3 == 0) {
+						result = "FizzBuzz";
+						return result;
+					}
+					else {
+						if (Integer.toString(number).contains("5")) {
+							result = "Buzz";
+							return result;
+						}
+						else {
+					result = "Buzz";
+					return result;
+						}
+					}
+				}
+			}
+			else {
+				result = Integer.toString(number);
+				// System.out.println(result);
+				return  result;
+			}	
+		}
+		else {
+			return "Not a valid entry.";
+		}
+	}
+	
+	public static void fizzBuzzOutput(int number) {
+		File newFile = new File("FizzBuzz.txt");
+		
+		try(PrintWriter newWriter = new PrintWriter(newFile)) {
+			for (int i = 0; i <= number; i++) {
+				newWriter.println(fizzBuzz(i));
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 }
