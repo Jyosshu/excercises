93a315e386b050ab11355e7f07ba1bf68d761583
Josh Wygle <jwygle@gmail.com>
2017-11-13 17:58:38 -0500

    committing

--- m4-w10d2-java-history_geek_exercises/src/main/webapp/js/exercises/api-exercises.js
+++ m4-w10d2-java-history_geek_exercises/src/main/webapp/js/exercises/api-exercises.js
@@ -1,5 +1,19 @@
 ﻿/// <reference path="../jquery-3.1.1.js" />
+
+var root = 'http://localhost:8080/m4-java-historygeek-exercises/';
+
 $(document).ready(function () {
     
-
+	$('#BillingPostalCode').on('change', function(event) {
+		$.ajax({
+			url: root + "/api/getTax",
+			method: 'GET',
+			data: {
+				'BillingPostalCode': $(this).val(),
+				'subtotal': $(this).val(),
+			}
+		}).then (function(data) {
+			
+		});
+	})
 });
