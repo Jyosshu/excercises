c072e19024357044c81cac967816d21be76e9f34
Josh Wygle <jwygle@gmail.com>
2017-11-30 15:43:35 -0500

    update to proj

--- m5-w11d5-java-critter-vulnerabilities/src/main/java/com/techelevator/critter/controller/UserController.java
+++ m5-w11d5-java-critter-vulnerabilities/src/main/java/com/techelevator/critter/controller/UserController.java
@@ -2,6 +2,8 @@ package com.techelevator.critter.controller;
 
 import java.util.Map;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
